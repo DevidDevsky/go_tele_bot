dd561ee6e5344b7c5a928a59bcd016e3db60f448
pupsmane <korjagin58@gmail.com>
2026-09-19 04:32:03 +0300

    add: start

--- handlers/commands.go
+++ handlers/commands.go
@@ -1,2 +1,25 @@
 package handlers
 
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func HandleCommands(ctx *HandlerContext) {
+	if ctx.Update.Message == nil {
+		return
+	}
+
+	switch ctx.Update.Message.Command() {
+	case "start":
+		msg := tgbotapi.NewMessage(
+			ctx.UserInfo.ChatID,
+			"Привет! 👋 Добро пожаловать в бота!",
+		)
+
+		if _, err := ctx.Bot.Send(msg); err != nil {
+			log.Println(err)
+		}
+	}
+}
