7f2796ac640927a3aecd82d917390da717348bc6
pupsmane <korjagin58@gmail.com>
2026-09-22 00:07:22 +0300

    fix: release Heandle for switch

--- handlers/commands.go
+++ handlers/commands.go
@@ -8,14 +8,35 @@ import (
 
 // функция которая отправяет что-то на определенную команду можешь через switch
 func HandleCommands(ctx *HandlerContext) {
-	if ctx.Update.Message.IsCommand() && ctx.Update.Message.Command() == "start" {
-		HandleStart(ctx)
+	if ctx.Update.Message.IsCommand() {
+		switch ctx.Update.Message.Command() {
+		case "start":
+			HandleStart(ctx)
+		}
+	} else {
+		msg := tgbotapi.NewMessage(ctx.UserInfo.ChatID, "Введите правильную команду!")
+		_, err := ctx.Bot.Send(msg)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
 func HandleStart(ctx *HandlerContext) {
 	log.Printf("%#v", ctx.UserInfo) //next to db
-	msg := tgbotapi.NewMessage(ctx.UserInfo.ChatID, "Прив)")
+	text := `👋 Добро пожаловать в ScheduleBot!
+
+	Я помогу вам всегда быть в курсе расписания — без лишних поисков и скриншотов.
+
+	Что я умею:
+	📅 Показывать расписание на сегодня, завтра и неделю
+	🔔 Напоминать о начале пар/встреч
+	🔄 Сообщать об изменениях и заменах
+	⚙️ Настраиваться под чётные/нечётные недели
+
+	Чтобы начать, выберите свою группу 👇`
+
+	msg := tgbotapi.NewMessage(ctx.UserInfo.ChatID, text)
 	_, err := ctx.Bot.Send(msg)
 	if err != nil {
 		log.Println(err)
